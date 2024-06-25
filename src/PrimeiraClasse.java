b76222fe1d3215c8c2f26e27c795f2b48a25f611
Neto Russo <netoacrn1@gmail.com>
2024-06-24 21:10:52 -0300

    subindo a pasta Primeira Classe

--- src/PrimeiraClasse.java
+++ src/PrimeiraClasse.java
@@ -1,5 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+/*Crie um projeto Java no IntelliJ que deve conter uma classe chamada Perfil, com o método main, que ao executar deverá imprimir um cumprimento personalizado no console. Por exemplo, "Olá, [Seu Nome]!".
+Modifique o programa para imprimir uma segunda mensagem de cumprimento personalizada. Por exemplo, "Tudo bem?".
+Crie uma classe chamada Estudos para imprimir anotações de estudos no console.
+Crie uma classe Soma em Java para imprimir no console o resultado da operação 10 + 5. Certifique-se de que o resultado seja exibido sem o uso de aspas, apresentando o valor "15" ao invés da expressão "10+5".
+Crie uma classe Subtração para imprimir o resultado de 10 - 5 no console.
+*/
+
+
+
 public class PrimeiraClasse {
 
     public static void main(String[] args) {
